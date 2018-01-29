f30c6ccc281c70554fe17a583881b75dc769a3e3
GitVerge <GitVerge@users.noreply.github.com>
2018-01-29 19:16:19 +0100

    Dot and Line basic methods.
     + jar utils update

--- src/dot/sx/Dot.java
+++ src/dot/sx/Dot.java
@@ -4,29 +4,96 @@ public class Dot {
 
 	private double x;
 	private double y;
-	
+	private int connections;
+	private boolean first;
+	private boolean last;
+
 	public Dot(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
 
 	public double getDist(Dot dot) {
-		//TODO
-		return 0.0;
+		return Math.sqrt( Math.pow(x - dot.getX(), 2) + Math.pow(y - dot.getY(), 2) );
 	}
 	
+	public boolean isValid() {
+		int max = (first || last) ? 1 : 2;
+		return connections <  max;
+	}
+	
+	@Override
+	public String toString() {
+		return "Dot [x=" + x + ", y=" + y + ", connections=" + connections + ", first=" + first + ", last=" + last
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(y);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dot other = (Dot) obj;
+		if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+			return false;
+		if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y))
+			return false;
+		return true;
+	}
+
 	public double getX() {
 		return x;
 	}
+	
 	public void setX(double x) {
 		this.x = x;
 	}
+	
 	public double getY() {
 		return y;
 	}
+	
 	public void setY(double y) {
 		this.y = y;
 	}
-	
-	
+
+	public int getConnections() {
+		return connections;
+	}
+
+	public void increaseConnections() {
+		++connections;
+	}
+
+	public boolean isFirst() {
+		return first;
+	}
+
+	public void setFirst(boolean first) {
+		this.first = first;
+	}
+
+	public boolean isLast() {
+		return last;
+	}
+
+	public void setLast(boolean last) {
+		this.last = last;
+	}
+
 }
--- src/dot/sx/Line.java
+++ src/dot/sx/Line.java
@@ -1,5 +1,7 @@
 package dot.sx;
 
+import java.util.Comparator;
+
 public class Line {
 
 	private Dot start;
@@ -9,24 +11,100 @@ public class Line {
 	public Line(Dot start, Dot end) {
 		this.start = start;
 		this.end = end;
+
 		setDist(start.getDist(end));
+	}
+
+	public int compareTo (Object o) {
+		Line other = (Line) o;
+
+		if (dist > other.dist) {
+			return 1;
+		} else if (dist < other.dist) {
+			return -1;
+		}
+
+		return 0;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(dist);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((end == null) ? 0 : end.hashCode());
+		result = prime * result + ((start == null) ? 0 : start.hashCode());
+		return result;
+	}
+
+	public boolean isInversion(Line l) {
+		return l.start.equals(end) && l.end.equals(start);
+	}
+	
+	public boolean containsLast() {
+		return start.isLast() || end.isLast();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Line other = (Line) obj;
+		if (Double.doubleToLongBits(dist) != Double.doubleToLongBits(other.dist))
+			return false;
+		if (end == null) {
+			if (other.end != null)
+				return false;
+		} else if (!end.equals(other.end))
+			return false;
+		if (start == null) {
+			if (other.start != null)
+				return false;
+		} else if (!start.equals(other.start))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Line [start=" + start + ", end=" + end + ", dist=" + dist + "]";
+	}
+
+	public boolean isValid() {
+		return start.isValid() && end.isValid();
+	}
+	
+	public void activate() {
+		start.increaseConnections();
+		end.increaseConnections();
 	}
 
 	public Dot getStart() {
 		return start;
 	}
+
 	public void setStart(Dot start) {
 		this.start = start;
 	}
+
 	public Dot getEnd() {
 		return end;
 	}
+
 	public void setEnd(Dot end) {
 		this.end = end;
 	}
+
 	public double getDist() {
 		return dist;
 	}
+
 	public void setDist(double dist) {
 		this.dist = dist;
 	}
